1cc691d1cd77ab8aab4e0e97f4463ba2b27efd15
DESKTOP-4J4MD87\PC <hmoi27509@gmail.com>
2023-07-15 16:18:27 +0700

    commit nv

---
+++ src/main/java/DomainModels/NhanVien.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DomainModels;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author PC
+ */
+public class NhanVien {
+     private String id;
+    private String ma;
+    private String ten;
+    private String gioiTinh;
+    private String ngaySinh;
+    private String diaChi;
+    private String matKhau;
+    private String email;
+    
+    @ManyToOne
+    @JoinColumn(name = "IdChucVu")
+    private ChucVu IdChucVu;
+    private String sdt;
+    private int trangThai;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String id, String ma, String ten, String gioiTinh, String ngaySinh, String diaChi, String matKhau, String email, ChucVu IdChucVu, String sdt, int trangThai) {
+        this.id = id;
+        this.ma = ma;
+        this.ten = ten;
+        this.gioiTinh = gioiTinh;
+        this.ngaySinh = ngaySinh;
+        this.diaChi = diaChi;
+        this.matKhau = matKhau;
+        this.email = email;
+        this.IdChucVu = IdChucVu;
+        this.sdt = sdt;
+        this.trangThai = trangThai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ChucVu getIdChucVu() {
+        return IdChucVu;
+    }
+
+    public void setIdChucVu(ChucVu IdChucVu) {
+        this.IdChucVu = IdChucVu;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public int getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(int trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    @Override
+    public String toString() {
+        return "NhanVien{" + "id=" + id + ", ma=" + ma + ", ten=" + ten + ", gioiTinh=" + gioiTinh + ", ngaySinh=" + ngaySinh + ", diaChi=" + diaChi + ", matKhau=" + matKhau + ", email=" + email + ", IdChucVu=" + IdChucVu + ", sdt=" + sdt + ", trangThai=" + trangThai + '}';
+    }
+
+    
+    
+}
